358b2976890f8c6fd4db030b52b1a56d962b8ed5
Vivek Kumar <er.vivekkumar21@gmail.com>
2017-07-28 00:34:24 +0530

    Angularjs source file added

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-// Runs when HTML file is loaded
-$(document).ready(function() {
-    $('#user-email').on('keypress',function() {
-        console.log($('#user-email').val())
-    });
-});
-
-// $('#user-email').on('keypress',function() {
-// $('#user-email').on('keyup',function() {
-$('#user-email').on('input',function() {
-        var email = $('#user-email').val()
-        var message = 'Welcome Back, ' + email;
-        $('.welcome-message').text(message);
-    });
+// // Runs when HTML file is loaded
+// $(document).ready(function() {
+//     $('#user-email').on('keypress',function() {
+//         console.log($('#user-email').val())
+//     });
+// });
+//
+// // $('#user-email').on('keypress',function() {
+// // $('#user-email').on('keyup',function() {
+// $('#user-email').on('input',function() {
+//         var email = $('#user-email').val()
+//         var message = 'Welcome Back, ' + email;
+//         $('.welcome-message').text(message);
+//     });
