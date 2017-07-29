5b1d027088668172914fa03e55cda91f2bcaa22e
Vivek Kumar <er.vivekkumar21@gmail.com>
2017-07-29 16:13:37 +0530

    Login page hidden and start working on main page

--- main.js
+++ main.js
@@ -1,14 +1,5 @@
-// // Runs when HTML file is loaded
-// $(document).ready(function() {
-//     $('#user-email').on('keypress',function() {
-//         console.log($('#user-email').val())
-//     });
-// });
-//
-// // $('#user-email').on('keypress',function() {
-// // $('#user-email').on('keyup',function() {
-// $('#user-email').on('input',function() {
-//         var email = $('#user-email').val()
-//         var message = 'Welcome Back, ' + email;
-//         $('.welcome-message').text(message);
-//     });
+var foodieApp = angular.module('foodieApp',[]);  //initilization of angular app
+
+foodieApp.controller('mainController',function($scope) {
+	$scope.restaurants = ['Farzi Cafe','Pizza Hut','Wenger\'s Deli','Sagar Ratna'];
+})
